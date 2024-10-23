567ba5be2a06ec8b242bebc32dc3bd40bbd468eb
abdelaaziz mostefaoui <azizmostfaouikmk@gmail.com>
2024-10-23 17:50:46 +0100

    Deploy React project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,10 @@
 import React from 'react';
+import "./Header.css"; // استيراد ملف CSS
 
 const Header = () => {
   return (
-    <div style={{ 
-      
-      display: "flex",  
-      alignItems: "center", 
-      background: "#1A1A40", 
-      width: "100%", 
-      padding: "5px", 
-      position: "fixed", 
-      top: 0, 
-      right: 1,
-      zIndex: 1000 ,
-    borderBottomLeftRadius:50,
-    borderBottomRightRadius:50
-    ,
-    }}>
-      <h2 style={{ color: "white", marginLeft: "700px", }}>
+    <div className="header"> {/* إضافة الفئة هنا */}
+      <h2>
           freelancer_2025 : abdelaaziz mostefaoui
       </h2>
     </div>
@@ -25,3 +12,4 @@ const Header = () => {
 };
 
 export default Header;
+
